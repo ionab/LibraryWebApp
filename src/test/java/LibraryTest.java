8d5d79c3412adbb59b8ed6f1f8b4df9957117870
AndrewLowrie <andrew2lowrie@googlemail.com>
2018-05-23 13:26:10 +0100

    Add passing test for library name

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,21 @@
+import models.Library;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    Library testLibrary;
+
+    @Before
+    public void before(){
+        testLibrary = new Library("The John Muir library");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("The John Muir library", testLibrary.getName());
+    }
+
 }
